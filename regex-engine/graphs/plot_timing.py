5e05e9c9fea62beada30d711ce3555654892428b
RootLeo00 <caterina.leonelli@gmail.com>
2023-10-07 11:55:42 +0200

    clean code

--- regex-engine/graphs/plot_timing.py
+++ regex-engine/graphs/plot_timing.py
@@ -1,57 +1,68 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# plt.style.use('style.mplstyle')
-
 def plot_timing(file, x, y):
-    
-    listdf=[]
-    name=[]
-    fig=plt.figure(figsize=(9, 9))
-    plt.subplots_adjust(left=0.25, bottom=0.15)
+    # Initialize lists to store dataframes and plot labels
+    listdf = []
+    name = []
+
+    # Create a figure for the plot
+    fig = plt.figure(figsize=(9, 9))
+    plt.subplots_adjust(left=0.25, bottom=0.15)  # Adjust subplot position
+
     print("plotting: ", file)
-    i=0
+    i = 0
+
+    # Loop through input files
     for f in file:
-        #if f ends with pkl, read pickle
+        # If the file ends with 'pkl', read it as a pickle file
         if f.split(".")[1] == "pkl":
             print("reading pickle: ", f.split(".")[1])
             listdf.append(pd.read_pickle(f))
+        # If the file ends with 'csv', read it as a CSV file
         if f.split(".")[1] == "csv":
             listdf.append(pd.read_csv(f))
-        # listdf.append(pd.read_pickle(f))
-        print(listdf[i])
 
-        #retrieve engine name and pattern
-        tmp=f.split("/")[-1]
+        # Retrieve engine name and pattern from the file path
+        tmp = f.split("/")[-1]
         name.append(tmp.split("_")[1])
         print("name", name)
 
-        #plot xtick every 10 elements 
-        df=listdf[i]
-        plt.scatter(df[x][::5], df[y][::5], label=name[i].upper(), marker='o')
-        plt.plot(df[x], df[y])
-        i+=1
+        # Plot xtick every 10 elements
+        df = listdf[i]
+        plt.scatter(df[x][::5], df[y][::5], label=name[i].upper(), marker='o')  # Scatter plot with labels
+        plt.plot(df[x], df[y])  # Line plot
+        i += 1
 
+    # Set the font size for x and y ticks
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
 
-    plt.legend()
-    plt.xlabel(x, fontsize=12)
-    plt.ylabel(y, fontsize=12)
+    plt.legend()  # Show legend
+    plt.xlabel(x, fontsize=12)  # Set x-axis label
+    plt.ylabel(y, fontsize=12)  # Set y-axis label
 
-    plt.savefig('./'+x+'_'+y+'.png')
+    # Save the plot as an image and display it
+    plt.savefig('./' + x + '_' + y + '.png')
     plt.show()
 
-
 if __name__ == "__main__":
-    # main()
-    file=[ "/home/leo/github/gutenberg-app/regex-engine/output/output_automa_patternlength.csv",
-          "/home/leo/github/gutenberg-app/regex-engine/output/output_kmp_patternlength.pkl",
+    # List of input files
+    file = [
+        "/home/leo/github/gutenberg-app/regex-engine/output/output_automa_patternlength.csv",
+        "/home/leo/github/gutenberg-app/regex-engine/output/output_kmp_patternlength.pkl",
         "/home/leo/github/gutenberg-app/regex-engine/output/output_radixtree_patternlength.pkl",
-          ]
-    
-    plot_timing(file,  "pattern_len","time_elapsed")
-    file=["/home/leo/github/gutenberg-app/regex-engine/output/output_automa_textlength.csv",
-          "/home/leo/github/gutenberg-app/regex-engine/output/output_radixtree_textlength.pkl",
-          "/home/leo/github/gutenberg-app/regex-engine/output/output_kmp_textlength.pkl"]
-    plot_timing( file, "ncharacters", "time_elapsed")
+    ]
+
+    # Create a plot for pattern length vs. time elapsed
+    plot_timing(file, "pattern_len", "time_elapsed")
+
+    # List of input files
+    file = [
+        "/home/leo/github/gutenberg-app/regex-engine/output/output_automa_textlength.csv",
+        "/home/leo/github/gutenberg-app/regex-engine/output/output_radixtree_textlength.pkl",
+        "/home/leo/github/gutenberg-app/regex-engine/output/output_kmp_textlength.pkl"
+    ]
+
+    # Create a plot for text length vs. time elapsed
+    plot_timing(file, "ncharacters", "time_elapsed")
